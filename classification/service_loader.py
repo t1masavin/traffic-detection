1532ec0b78c69aad489fff01cbe2b898551d0514
tim <timasaviin@gmail.com>
2023-01-02 21:11:50 +0700

    new parser for classification

--- classification/service_loader.py
+++ classification/service_loader.py
@@ -1,8 +1,18 @@
 import sys
 from pathlib import Path
+import re
 
 
 def  sys_append_abs():
     file = Path('__file__').resolve()
     parent, root = file.parent, file.parents[1]
     sys.path.append(str(root))
+
+
+
+def clear_file(item):
+    with open(item, 'r') as file:
+        clear_file = re.sub('["\{\}]', '', file.read())
+
+    with open(item, 'w') as file:
+        file.write(clear_file)
